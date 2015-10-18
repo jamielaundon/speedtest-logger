56a4327d8863155780bd18ca67c1721b3a5789ae
jamielaundon <Jamie@Laundon.org>
2015-10-18 17:00:59 +0100

    Added dropdown range selection and tickbox to hide ping data

--- src/speedtest-logger/Controllers/HomeController.cs
+++ src/speedtest-logger/Controllers/HomeController.cs
@@ -13,9 +13,10 @@ namespace speedtest_logger.Controllers
     {
         public ActionResult Index()
         {
-            speedtest_common.Repositories.TestRepository repo = new speedtest_common.Repositories.TestRepository(speedtest_common.dbConfig.create(ConfigurationManager.ConnectionStrings["speedtest_logger.Properties.Settings.logConnectionString"].ConnectionString));
-            IEnumerable<speedtest_common.Objects.Result> results = repo.getResultsDays(7); // Get 7 days of results by default
-            return View(results);
+            //speedtest_common.Repositories.TestRepository repo = new speedtest_common.Repositories.TestRepository(speedtest_common.dbConfig.create(ConfigurationManager.ConnectionStrings["speedtest_logger.Properties.Settings.logConnectionString"].ConnectionString));
+            //IEnumerable<speedtest_common.Objects.Result> results = repo.getResultsDays(7); // Get 7 days of results by default
+            //return View(results);
+            return View(); // V2 We now return an empty View.  The data is loaded via Ajax instead.
         }
     }
 }
